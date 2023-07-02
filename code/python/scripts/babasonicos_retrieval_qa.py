10fc0497258f1c7d57c60d6c9a561e6e3567eaa1
rodralez <rodralez@gmail.com>
2023-07-02 10:27:58 -0300

    retrieval qa example, v1.3

--- code/python/scripts/babasonicos_retrieval_qa.py
+++ code/python/scripts/babasonicos_retrieval_qa.py
@@ -1,17 +1,33 @@
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-#from langchain.document_loaders import DirectoryLoader
-from langchain.document_loaders import UnstructuredODTLoader
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.embeddings import SentenceTransformerEmbeddings
-from langchain.text_splitter import NLTKTextSplitter
-import os
-import sys
+'''
+## Retrieval-based QA System for Babasónicos Wikipedia Page
+
+This script implements a retrieval-based question-answering (QA) system using the information from the Babasónicos Wikipedia page. Babasónicos is a rock band from Argentina, and the script utilizes various technologies to provide answers to user queries. The script makes use of the following technologies:
+
+1. langchain: A framework for developing applications powered by language models.
+2. SentenceTransformer: A library for generating sentence embeddings.
+3. NLTKTextSplitter: A tool for splitting the text into smaller chunks.
+4. Chroma DB: A database for storing the text chunks and their corresponding embeddings.
+5. Text Generator UI: A user interface for interacting with the language model.
+
+To use this script, ensure that the required dependencies are installed. The script is designed to retrieve information specifically from the Babasónicos Wikipedia page. Queries can be entered via the Text Generator UI, and the script will provide relevant answers based on the available information.
+
+Please note that the LLM (Large Language Model) has no prior knowledge about Babasónicos or its Wikipedia page. All answers are generated based on the information contained within the page itself.
+
+For further assistance or inquiries, refer to the script documentation or contact the script maintainer.
+'''
 
 # Before running this script, you have to run the following commands in the terminal:
 # ~$ pip install -r $PROJECT_PATH/requirements.txt
 # ~$ cd text-generation-webui/
 # ~$ python server.py --verbose --api
+# Then, load the Vicuna 13B model in the UI.
+
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.text_splitter import NLTKTextSplitter
+import os
+import sys
 
 # Define the path to the project folder
 PROJECT_PATH = os.path.join(os.getenv('HOME'), 'JurisGPT')
@@ -28,9 +44,6 @@ import llm_additional_functions as ad
 #%% EMBEDDEDING
 # Define embedding function
 embedding_fnc = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-# embedding_fnc = SentenceTransformerEmbeddings(model_name="multi-qa-mpnet-base-dot-v1")
-# embedding_fnc = SentenceTransformerEmbeddings(model_name="sentence-transformers/paraphrase-MiniLM-L3-v2")
-# embedding_fnc = SentenceTransformerEmbeddings(model_name="sentence-transformers/distiluse-base-multilingual-cased-v1")
 
 #%% LOAD DATA
 # load data file
@@ -40,7 +53,6 @@ docs = loader.load()
 #%% SPLIT DOCUMENTS IN CHUNKS 
 # https://www.pinecone.io/learn/chunking-strategies/
 text_splitter = NLTKTextSplitter(chunk_size=1000, chunk_overlap=0)
-# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
 docs_split = text_splitter.split_documents(docs)
 
@@ -67,6 +79,7 @@ docs_query = db.similarity_search(question, k=3)
 context = '\n'.join([doc.page_content for doc in docs_query])
 
 #%% PROMPT
+# header_en = "### Assistant: I use the following context to answer the question. If I don't know the answer, I'll simply say that I don't know it, I won't try to invent an answer."
 header = "### Assistant: uso el siguiente contexto para responder la pregunta. Si no se la respuesta, solo diré que no la se, no trataré de inventar una respuesta."
 prompt = ad.format_prompt(header, question, context)
 
@@ -83,4 +96,4 @@ print ("Human question: ", question)
 print ("LLM response: ", response)
 print(f"LLM response time: {elapsed_time:.3f} seconds.")
 
-dumb = 0
+dumb_break = 0
